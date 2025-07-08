1fafcad7bb8058ba4905f93f5910c4227fb647fb
cuong pham <cuongdtnt109@gmail.com>
2025-07-08 14:26:27 +0700

    update next chapter

--- resources/views/client/chapters/show.blade.php
+++ resources/views/client/chapters/show.blade.php
@@ -525,10 +525,24 @@ document.querySelectorAll('input[name="vip_package"]').forEach(function(radio){
 
 </script>
 <script>
-    $(document).ready(function () {
-        @if($redirectToAffiliate && !empty($redirectAffiliateLink))
-            window.open("{{ $redirectAffiliateLink }}", "_blank");
-        @endif
+    document.addEventListener("DOMContentLoaded", function () {
+        const nextBtn = document.getElementById('next_chap');
+
+        if (nextBtn && !nextBtn.classList.contains('disabled')) {
+            nextBtn.addEventListener('click', function (e) {
+                const affiliateLink = nextBtn.getAttribute('data-affiliate-link');
+                const nextHref = nextBtn.getAttribute('href');
+
+                if (affiliateLink) {
+                    window.open(affiliateLink, '_blank');
+                }
+
+                window.location.href = nextHref;
+
+                e.preventDefault();
+            });
+        }
     });
 </script>
+
 @endsection
